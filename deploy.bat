9d6d5ed08d5f77a82cd76467bb5e1bdbea5985b0
suveng <suveng@163.com>
2018-12-25 13:45:37 +0800

    deploy at 2018/12/25 周二 13:45:31.67

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-git add . && git commit -m "deploy at %date% %time%" && git push origin source
-=======
-git add . &&git commit -m  "auto deploy %date% %time%" &&git push origin source
->>>>>>> 4b136844b2af433cd86f20aee8d812c2176d18f2
+java -jar ./blog/theme/sitemapGen.jar && git add . -A && git commit -m "deploy at %date% %time%" && git push origin source
