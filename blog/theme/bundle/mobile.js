0a555631d5d683fb6eea16e69f3d6cc4b0a2911e
suveng <suveng@163.com>
2019-01-05 16:52:56 +0800

    deploy at 2019-01-05 16:52:40

--- blog/theme/bundle/mobile.js
+++ blog/theme/bundle/mobile.js
@@ -1,13 +1,17 @@
 //  移动端适配
 (function() {
     'use strict';
-    console.log("mobile.js");
-    //动态获取窗口大小
-    var body = (document.compatMode && document.compatMode == 'CSS1Compat') ? document.documentElement : document.body;
-    var bodyWidth = body.offsetWidth;
-    if (bodyWidth<500) {
-        //宽度改为100%
-        document.getElementsByClassName("main")[0].style.maxWidth="100%";
-    }
+
+    $(document).ready(function() {
+        console.log("mobile.js ready!");
+        //动态获取窗口大小
+        var body = (document.compatMode && document.compatMode == 'CSS1Compat') ? document.documentElement : document.body;
+        var bodyWidth = body.offsetWidth;
+        if (bodyWidth < 500) {
+            //宽度改为100%
+            document.getElementsByClassName("main")[0].style.maxWidth = "100%";
+            console.log("移动端适配完成！");
+        }
+    });
 
 })();
