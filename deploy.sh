a10cdb69b66d0a3e2d383960961b34118fae1d27
suveng <suveng@163.com>
2019-02-15 14:29:07 +0800

    deploy at 2019-02-15 14:29:05

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ cd blog/theme
 java -jar sitemapGen.jar
 cd ../../
 git config core.autocrlf
+git pull origin source
 git add . -A
 git commit -m "deploy at ${time}"
 git push origin source 
--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 time=$(date "+%Y-%m-%d %H:%M:%S")
 git config core.autocrlf
+git pull origin source 
 git add . -A
 git commit -m "deploy at ${time}"
 git push origin source 
